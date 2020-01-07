772fe2e231950e964bf055bec18c6c3b95e04a7b
Shahar Mintz <smintz@protoconf.com>
2020-01-07 21:48:39 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM protoconf/protoconf:v0.1.0-pre2 AS protoconf
+FROM ubuntu:latest
+
+COPY --from=protoconf /app/cmd/protoconf/protoconf /protoconf
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+STORE_TYPE=$1
+STORE_ADDRESS=$2
+PREFIX=$3
+SHOULD_DELETE=$4
+CHANGES_ONLY=$5
+export GIT_TERMINAL_PROMPT=0
+
+if [ "${CHANGES_ONLY}" == "true" ]; then
+  CONFIGS_TO_INSERT=$(git diff --name-only --diff-filter=ACMR HEAD~1 HEAD | sed -n 's/^materialized_config\/\(.*\.materialized_JSON\)$/\1/p')
+else
+  CONFIGS_TO_INSERT=$(find . -name *.materialized_JSON | sed -n 's/^materialized_config\/\(.*\.materialized_JSON\)$/\1/p')
+fi
+if [ -z "$CONFIGS_TO_INSERT" ]; then
+    exit 0
+fi
+DELETE=""
+if [ "${SHOULD_DELETE}" == "true" ]; then
+  DELETE="-d"
+fi
+
+echo $CONFIGS_TO_INSERT | xargs /protoconf insert \
+  -store ${STORE_TYPE} \
+  -address ${STORE_ADDRESS} \
+  -prefix ${PREFIX} \
+  ${DELETE} \
+  "$GITHUB_WORKSPACE"
