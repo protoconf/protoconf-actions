73b1568c16e847065414fd2c2f47a2af1a60351c
Shahar Mintz <smintz@protoconf.com>
2020-01-07 22:35:10 +0000

    do not strip the .materialized_JSON postfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ CHANGES_ONLY=$5
 export GIT_TERMINAL_PROMPT=0
 
 if [ "${CHANGES_ONLY}" == "true" ]; then
-  CONFIGS_TO_INSERT=$(git diff --name-only --diff-filter=ACMR HEAD~1 HEAD | sed -n 's/^materialized_config\/\(.*\.materialized_JSON\)$/\1/p')
+  CONFIGS_TO_INSERT=$(git diff --name-only --diff-filter=ACMR HEAD~1 HEAD | sed -n 's/^materialized_config\///p')
 else
-  CONFIGS_TO_INSERT=$(find . -name *.materialized_JSON | sed -n 's/^.\/materialized_config\/\(.*\.materialized_JSON\)$/\1/p')
+  CONFIGS_TO_INSERT=$(find . -name *.materialized_JSON | sed -n 's/^.\/materialized_config\///p')
 fi
 if [ -z "$CONFIGS_TO_INSERT" ]; then
     exit 0
@@ -26,6 +26,6 @@ fi
 echo $CONFIGS_TO_INSERT | xargs /protoconf insert \
   -store "${STORE_TYPE}" \
   -store-address "${STORE_ADDRESS}" \
-  -prefix "${PREFIX}" \
+  -prefix "${PREFIX}/" \
   ${DELETE} \
   "$GITHUB_WORKSPACE"
